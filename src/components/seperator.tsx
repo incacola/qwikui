d3c8dd50eba3dbad2c5fff14ccc474c92651d138
Felipe Gil Gutierrez <felipe.gil@arvato.com>
2023-05-11 19:57:08 +0200

    Separetor work in progress

---
+++ src/components/seperator.tsx
@@ -0,0 +1,7 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() =>{
+  return (
+    <div class="w-full h-[1px] bg-gradient-to-r from-white from-89% via-white/80 via-49% to-white"></div>
+  )
+})
--- src/routes/docs/[...slug]/index.tsx
+++ src/routes/docs/[...slug]/index.tsx
@@ -1,5 +1,34 @@
 import { component$ } from "@builder.io/qwik";
+import { cn } from "~/lib/utils"
+import Separator from "~/components/seperator"
 
 export default component$(() =>{
-  return <div>Docs</div>
+  const doc = {}
+
+  return (
+    <main class="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
+      <div class="mx-auto w-full min-w-0">
+        <div class="mb-4 flex items-center space-x-1 text-sm text-muted-foreground">
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap">
+            Docs
+          </div>
+
+          <div class="font-medium text-foreground">Doc Title</div>
+        </div>
+        <div class="space-y-2">
+          <h1 class={cn("scroll-m-20 text-4xl font-bold tracking-tight")}>
+            Document Title
+          </h1>
+            <p class="text-lg text-muted-foreground">
+              Doc description
+            </p>
+        </div>
+        <div class="my-4 md:my-6">
+          <Separator  />
+        </div>
+        
+        Docs content
+      </div>
+    </main>
+  )
 })
